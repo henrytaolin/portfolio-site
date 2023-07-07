18ea6271dc329d5ac57faf563cbfc8ab5cb7c21c
Nana Henry <henrytaolin@gmail.com>
2023-07-07 10:58:24 +0700

    2nd push

--- js/script.js
+++ js/script.js
@@ -3,17 +3,17 @@ $(document).ready(function() {
     $('#menu').click(function(){
         $(this).toggleClass('fa-times');
         $('header').toggleClass('toggle');
+    });
+
+    $(window).on('scroll load', function(){
+        $('#menu').removeClass('fa-times');
+        $('header').removeClass('toggle');
 
         if($(window).scrollTop() > 0) {
             $('.top').show();
         } else {
             $('.top').hide();
         }
-    });
-
-    $(window).on('scroll load', function(){
-        $('#menu').removeClass('fa-times');
-        $('header').removeClass('toggle');
     });
 
 
