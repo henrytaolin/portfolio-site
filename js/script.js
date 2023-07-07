8577e2973a1719dc43e0f1821320417cf8e08c73
Nana Henry <henrytaolin@gmail.com>
2023-07-07 16:01:27 +0700

    update button effect

--- js/script.js
+++ js/script.js
@@ -28,4 +28,18 @@ $(document).ready(function() {
             'linear'
         );
     });
+
+
+    // button ripple effect
+
+    const btnE1 = document.querySelector(".btn");
+
+    btnE1.addEventListener("mouseover", (event) => {
+        const x = event.pageX - btnE1.offsetLeft;
+        const y = event.pageY - btnE1.offsetTop;
+
+        btnE1.style.setProperty("--xPos", x + "px");
+        btnE1.style.setProperty("--yPos", y + "px");
+    });
+
 });
